aa4134c6e5a80e785913e2791cf12966213016f7
THARUNKUMAR S <tharunsathy@gmail.com>
2025-03-19 16:45:03 +0530

    Delete New_docker_push-main directory

--- New_docker_push-main/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-IMAGE_NAME="tharun19stk/docker_stk"
-TAG="latest"
-
-# Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
-echo "Docker image $IMAGE_NAME:$TAG built successfully."
